eab73fff524e40b6b6c9310dba3dd480521f15a4
twwoface <twwoface444@gmail.com>
2025-02-19 10:28:34 +0530

    done

--- app.py
+++ app.py
@@ -7,23 +7,32 @@ users = {
     "test@gmail.com": "password123"
 }
 
-@app.route('/')
+@app.route('/',methods=['POST', 'GET'])
 def home():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+         return render_template('login.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if email in users and users[email] == password:
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('login.html', error="Invalid Credentials. Try again.")
+    
+    # If it's a GET request, just show the login page
     return render_template('login.html')
 
-@app.route('/login', methods=['POST'])
-def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    if email in users and users[email] == password:
-        return redirect(url_for('dashboard'))
-    else:
-        return "Invalid Credentials. Try again."
 
 @app.route('/dashboard')
 def dashboard():
-    return "<h1>Welcome to CanTech Dashboard</h1>"
+    return render_template('dashboard.html')  # Renders the dashboard page
 
 if __name__ == '__main__':
     app.run(debug=True)
